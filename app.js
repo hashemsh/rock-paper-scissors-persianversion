7f6d5ab75ba767c5a3d70e3c4232735f435ce332
hashemsh <sh.sheikhypour@gmail.com>
2019-12-22 12:20:29 +0330

    make variable for all element

---
+++ app.js
@@ -0,0 +1,12 @@
+let userScore = 0;
+let compScore = 0;
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector("score_board");
+const result_p = document.querySelector("result>p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+
+
