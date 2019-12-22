126e1564c803a6c4ee5de0e4060bbddf767d22c6
hashemsh <sh.sheikhypour@gmail.com>
2019-12-22 12:41:17 +0330

    make game function and specifie rules

--- app.js
+++ app.js
@@ -8,5 +8,41 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+function getCompuetrChoice(){
+    const choices = ['r','p','s'];
+    const randomNum = Math.floor(Math.random()*3);
+    return choices[randomNum];
+}
+function game(userChoice){
+    const compChoice = getCompuetrChoice();
+    switch(userChoice+compChoice){
+        case "rs":
+        case "pr":
+        case "sp":
+            console.log("user win ");
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            console.log("user lose ");
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            console.log("it's draw ");
+            break;    
+    }
+}
 
-
+function main(){
+    rock_div.addEventListener('click',function(){
+        game("r");
+    })
+    paper_div.addEventListener('click',function(){
+        game("p");
+    })
+    scissors_div.addEventListener('click',function(){
+        game("s");
+    })
+}
+main();
